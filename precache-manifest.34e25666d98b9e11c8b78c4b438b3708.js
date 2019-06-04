2eb047f06aa22bf17afbede42a0921d6910364a6
Harsh Joshi <joshih1998@gmail.com>
2019-06-04 11:51:12 +0530

    Updates

---
+++ precache-manifest.34e25666d98b9e11c8b78c4b438b3708.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cee52ba1219f1d9fe0fd92021044383",
+    "url": "/reduxreactmycv/index.html"
+  },
+  {
+    "revision": "480b670009e7573bddcd",
+    "url": "/reduxreactmycv/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ebee732b0a1883fde725",
+    "url": "/reduxreactmycv/static/css/main.807a75b4.chunk.css"
+  },
+  {
+    "revision": "480b670009e7573bddcd",
+    "url": "/reduxreactmycv/static/js/2.1107a318.chunk.js"
+  },
+  {
+    "revision": "ebee732b0a1883fde725",
+    "url": "/reduxreactmycv/static/js/main.0c207443.chunk.js"
+  },
+  {
+    "revision": "ba4715c1e3c3fdbdced1",
+    "url": "/reduxreactmycv/static/js/runtime~main.453e5018.js"
+  },
+  {
+    "revision": "95e20e3e84047a65108190404e40c347",
+    "url": "/reduxreactmycv/static/media/SP.95e20e3e.jpg"
+  },
+  {
+    "revision": "f7c526926ff1f5f8d8fb3ea0ee7f82ae",
+    "url": "/reduxreactmycv/static/media/logo.f7c52692.jpg"
+  }
+]);
